9d6fa9c6f098bba9874adc47d8ab18d4708b950a
Puneet Dhiman <puneetdhiman56@gmail.com>
2023-03-16 22:09:31 +0530

    API basics

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
+use rocket::{
+    http::Status,
+    serde::json::{serde_json::json, Value},
+};
+
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> String {
+    String::from("Hello world")
+}
+
+#[get("/hello/<name>")]
+fn greet_name(name: Option<String>) -> String {
+    let greeting = match name {
+        Some(n) => n,
+        None => String::from("No name creature"),
+    };
+
+    format!("Hello {}", greeting)
+}
+
+struct User {
+    name: String,
+}
+
+#[get("/json")]
+fn get_json() -> Result<Value, Status> {
+    Ok(json!({
+        "name":"puneet"
+    }))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, greet_name, get_json])
 }
